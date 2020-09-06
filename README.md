d01b9b33e99acae7f69fe2396fb79eff0f0eca8e
m-agahi <m.agahi@outlook.com>
2020-09-06 22:16:34 +0430

    11

--- README.md
+++ README.md
@@ -9,18 +9,34 @@
 1. First install Jenkins' [Metrics Plugin](https://wiki.jenkins-ci.org/display/JENKINS/Metrics+Plugin) and create a access key.
 2. Put Jenkins’ URL and access key into `jenkins-metrics.py`
 3. Move `jenkins-metrics.py` and `jenkins_zabbix.conf` to Jenkins master instance.
-4. Now take another look at Jenkins' [Metrics Plugin](https://wiki.jenkins-ci.org/display/JENKINS/Metrics+Plugin) to know more about these metrics and start creating items in Zabbix server
+4. Move `jenkins_zabbix.conf` to /etc/zabbix/zabbix_agentd.conf.d
+5. Move `jenkins-metrics.py` to /usr/local/bin/
+6. Make sure you have `python v 2` Installed 
+7. Restart `zabbix-agent` service 
+8. login to your Zabbix admin panel and import `zabbix5 jenkins template.xml` to `Configuration > Templates` 
 
 ---
 
 ## Appendix
+### Further Reading
+Take another look at Jenkins' [Metrics Plugin](https://wiki.jenkins-ci.org/display/JENKINS/Metrics+Plugin) to know more about these metrics and start creating items in Zabbix server
+
+### How to generate your own template
+You can use the bash scripts provided by `Mohammadmahmoud Agahi` to create your own customized template:
+1. On the Jenkins master node open a terminal and enter this command:
+        `python2 /usr/local/bin/jenkins-metrics.py | sort -u > ~/raw_jenkins_metric_data.txt`
+2. Look at the file get familiar with the keys
+3. edit the `make-template.sh` and edit it to fit your needs
+4. On the Jenkins master node open a terminal and enter this command: 
+        `./make-template.sh ~/raw_jenkins_metric_data.txt > jenkins-template.xml`
+
 ### How this python script works
 It simply uses Jenkins' [Metrics Plugin](https://wiki.jenkins-ci.org/display/JENKINS/Metrics+Plugin)’s API to get a Json containing metrics and transforms it in a more readable file to Zabbix.
 
 However you can also make use of it without a Zabbix, like shown bellow:
 
 ```
-python jenkins-metrics.py
+python2 jenkins-metrics.py
 ```
 
 This will return all the metrics at once. If you want to get just one of the metrics, use a full path to a metric item. For example, to get this metric
@@ -37,5 +53,5 @@ This will return all the metrics at once. If you want to get just one of the met
 You can type:
 
 ```
-python jenkins-metrics.py gauges.jenkins.executor.count.value.value
+python2 jenkins-metrics.py gauges.jenkins.executor.count.value.value
 ```
--- make-template.sh
+++ make-template.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-####################################################################################
+################################################################################
 #	This bash scritp will generate the xml template for zabbix version 5.0     #
 #	The input file is the export of metrics from the Jenkins Metrics Plugin    #
 #	The input file could be generated using the script provided by "   "       #
 #	                         DO NOT USE                                        #
 #	                ******still in progres*****                                #
-#	written by Mohammadmahmoud Agahi -                                         #
-####################################################################################
+#	               written by Mohammadmahmoud Agahi							   #
+#                                                                              #
+################################################################################
 
 ## The following Variables will determine which keywords to be ignored in the metrics file
 b1="units="
@@ -26,61 +27,61 @@ while IFS= read -r line
 		application=`echo $line | cut -d "." -f 2,3 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
     	metric=$line
     	name=`echo $line | cut -d "." -f 4,5 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g" | cut -d "=" -f 1`
-		units=0 
-	    vtype=0
-		name_addition=0
+		units="0" 
+	    vtype="0"
+		name_addition=""
+
 		if  echo $line | egrep "$b1|$b2|$b3|$b4|$b5|$b6|$b7|$b8|$b9"   > /dev/null
 		then
     		continue
-		elif [ `echo $line |  grep -o "\." | wc -l` -eq 3 ] 
+		elif [ `echo $line |  grep -o "\." | wc -l` -eq 3 ]
 		then
-       		application=`echo $line | cut -d "." -f 1 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
-       		name=`echo $line | cut -d "." -f 2,3  | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
-# gauge monitoring parameters
-		elif echo $line | grep "gauge" > /dev/null
+			application=`echo $line | cut -d "." -f 1 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+			name=`echo $line | cut -d "." -f 2,3  | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+		elif [ `echo $line |  grep -o "\." | wc -l` -eq 4 ]
 		then
-			if [ `echo $line |  grep -o "\." | wc -l` -eq 4 ]
-			then
-	       		application=`echo $line | cut -d "." -f 2,3 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
-    	   		name=`echo $line | cut -d "." -f 3,4  | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
-			elif [ `echo $line |  grep -o "\." | wc -l` -eq 5 ]
-			then
-	       		application=`echo $line | cut -d "." -f 2,3 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
-    	   		name=`echo $line | cut -d "." -f 3,4,5  | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
-			elif [ `echo $line |  grep -o "\." | wc -l` -eq 6 ]
-			then
-				application=`echo $line | cut -d "." -f 2,3,4 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
-    	   		name=`echo $line | cut -d "." -f 5,6  | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
-			else
-				continue
-			fi
-		elif  echo $line | grep "\.rate" | grep "mean" > /dev/null
+			application=`echo $line | cut -d "." -f 2,3 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+			name=`echo $line | cut -d "." -f 3,4  | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+		elif [ `echo $line |  grep -o "\." | wc -l` -eq 5 ]
 		then
+			application=`echo $line | cut -d "." -f 2,3 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+			name=`echo $line | cut -d "." -f 3,4,5  | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+		elif [ `echo $line |  grep -o "\." | wc -l` -eq 6 ]
+		then
+			application=`echo $line | cut -d "." -f 2,3,4 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+			name=`echo $line | cut -d "." -f 5,6  | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+		elif [ `echo $line |  grep -o "\." | wc -l` -eq 7 ]
+		then
+			application=`echo $line | cut -d "." -f 2,3,4,5 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+			name=`echo $line | cut -d "." -f 6,7  | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+		else
+			continue
+		fi
+		
+		if echo $line | grep "\.rate" | grep "mean" > /dev/null; then
    			units=`grep $(echo $line | cut -d "." -f 1,2,3,4,5) $1 | grep unit | cut -d "=" -f 2` 
 	   		vtype="FLOAT"
     		name_addition=" (mean rate)"
-		elif echo $line | grep "count" > /dev/null
-		then
-   			if echo $line | grep "duration" > /dev/null
-	    	then
+		elif echo $line | grep "count" > /dev/null; then
+   			if echo $line | grep "duration" > /dev/null; then
 				units="seconds"
        		fi
-		elif echo $line | grep mean_rate > /dev/null
-		then
+		elif echo $line | grep mean_rate > /dev/null; then
     			vtype="FLOAT"
     			units="seconds"
     			name_addition=" (mean)"
 		else
     		continue
 		fi
+
 		echo "				<item>
                 	<name>$name$name_addition</name>
                 	<key>jenkins.metrics[$metric]</key>"
-		if [ $vtype -ne 0 ]
+		if [[ $vtype != "0" ]]
 		then
             echo "					<value_type>$vtype</value_type>"
         fi
-		if [ $units -ne 0 ]
+		if [[ $units != "0" ]]
 		then
 		    echo "					<units>$units</units>"
 		fi
@@ -99,16 +100,34 @@ function extract_applications {
 	while IFS= read -r line 
 	do
    		application=`echo $line | cut -d "." -f 2,3 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+        
 		if  echo $line | egrep "$b1|$b2|$b3|$b4|$b5|$b6|$b7|$b8|$b9"   > /dev/null
 		then
-   			continue
-		elif [ `echo $line |  grep -o "\." | wc -l` -eq 3 ] 
+    		continue
+		elif [ `echo $line |  grep -o "\." | wc -l` -eq 3 ]
 		then
-   			application=`echo $line | cut -d "." -f 1 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+			application=`echo $line | cut -d "." -f 1 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+		elif [ `echo $line |  grep -o "\." | wc -l` -eq 4 ]
+		then
+			application=`echo $line | cut -d "." -f 2,3 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+		elif [ `echo $line |  grep -o "\." | wc -l` -eq 5 ]
+		then
+			application=`echo $line | cut -d "." -f 2,3 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+		elif [ `echo $line |  grep -o "\." | wc -l` -eq 6 ]
+		then
+			application=`echo $line | cut -d "." -f 2,3,4 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+		elif [ `echo $line |  grep -o "\." | wc -l` -eq 7 ]
+		then
+			application=`echo $line | cut -d "." -f 2,3,4,5 | sed 's/\./ /g' | sed -e "s/\b\(.\)/\u\1/g"`
+		else
+			continue
 		fi
+
         echo $application >> /tmp/application_temp
 	done < $1
+
 	cat /tmp/application_temp | sort -u | grep -v "0" > /tmp/application_temp1
+	
 	while IFS= read -r application 
 	do
 		echo "                <application>
